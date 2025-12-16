5b995698956218f2059cd85ca2a67b311bff55be
manelly67 <manellylopezqqq@gmail.com>
2025-12-15 20:58:37 -0500

    buttons to switch the language exercises

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ToggleTheme } from '../toggle-theme/toggle-theme';
 
@@ -8,6 +8,39 @@ import { ToggleTheme } from '../toggle-theme/toggle-theme';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
+
+  btn1:string="español";
+  btn2:string="portugués";
+
+  es:boolean=true;
+  pt:boolean=false;
+  mainTitle:string="";
+
+  ngOnInit() {
+    // Apply initial theme
+    this.setContent();
+  }
+
+  setContent(){
+    if(this.es){
+     this.mainTitle = "Exercises to practice the Spanish language"
+    }
+    if(this.pt){
+      this.mainTitle = "Exercises to practice the Portuguese language"
+    }
+  }
+
+  setSP(){
+    this.es = true;
+    this.pt = false;
+    this.setContent();
+  }
+
+  setPT(){
+    this.pt = true;
+    this.es = false;
+    this.setContent();
+  }
   
 }
