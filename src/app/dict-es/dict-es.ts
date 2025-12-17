de4189489353f243f37e44ff3d30b5dc10ba43cf
manelly67 <manellylopezqqq@gmail.com>
2025-12-17 18:07:01 -0500

    in dict-es dict-pt navbar behaviour

--- src/app/dict-es/dict-es.ts
+++ src/app/dict-es/dict-es.ts
@@ -1,24 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink, Router } from '@angular/router';
+import { Location } from '@angular/common';
 import { TopInPage } from '../top-in-page/top-in-page';
+import { Categoria } from '../categoria';
+import { MsgCateg } from '../msg-categ';
 
 @Component({
   selector: 'app-dict-es',
-  imports: [RouterLink,TopInPage],
+  imports: [RouterLink, TopInPage],
   templateUrl: './dict-es.html',
   styleUrl: './dict-es.css',
 })
-export class DictES implements OnInit{
+export class DictES implements OnInit {
   es: boolean = true;
   pt: boolean = false;
   mainTitle: string = "Exercises to practice the Spanish language";
   categ?: number;
+  filterObj?: Categoria;
+  arrOpt = [
+    { id: 1, url: "/dictadoEs/1", text: 'Beginner' },
+    { id: 2, url: "/dictadoEs/2", text: 'Intermediate' },
+    { id: 3, url: "/dictadoEs/3", text: 'Advanced' }
+  ];
+  arr?: Array<{ id: number, url: string, text: string }>;
 
-  constructor(public route: ActivatedRoute) { }
+  constructor(
+    public route: ActivatedRoute,
+    public router: Router,
+    public msgCateg: MsgCateg,
+    private location: Location,
+  ) { }
+
+  categoria = this.msgCateg.categoria;
+  categorias = this.msgCateg.captarCategorias();
+
   ngOnInit() {
     const categGet = this.route.snapshot.paramMap.get('categ');
     if (categGet) {
       this.categ = Number(categGet);
+      [this.filterObj] = this.categorias.filter((e) => e.id === this.categ);
+      this.arr = this.arrOpt.filter((e) => e.id !== this.categ);
     }
   }
+
+   /* functions necessary for reload the page by new params */
+  goBack(): void {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.location.back();
+    });
+  }
+
+  navigateToA(): void{
+    if(this.arr){
+      const newRoute= this.arr[0].url;
+      this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate([newRoute]);
+      });
+    }
+   }
+   navigateToB(): void{
+    if(this.arr){
+      const newRoute= this.arr[1].url;
+      this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate([newRoute]);
+      });
+    }
+   }
+
+  
+
 }
--- src/app/dict-pt/dict-pt.ts
+++ src/app/dict-pt/dict-pt.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink, Router } from '@angular/router';
+import { Location } from '@angular/common';
 import { TopInPage } from '../top-in-page/top-in-page';
+import { Categoria } from '../categoria';
+import { MsgCateg } from '../msg-categ';
 
 @Component({
   selector: 'app-dict-pt',
@@ -13,12 +16,54 @@ export class DictPT implements OnInit{
   pt: boolean = true;
   mainTitle: string = "Exercises to practice the Portuguese language";
   categ?: number;
+  filterObj?: Categoria;
+  arrOpt = [
+    { id: 11, url: "/dictadoEs/11", text: 'Beginner' },
+    { id: 12, url: "/dictadoEs/12", text: 'Intermediate' },
+    { id: 13, url: "/dictadoEs/13", text: 'Advanced' }
+  ];
+  arr?: Array<{ id: number, url: string, text: string }>;
 
-  constructor(public route: ActivatedRoute) { }
-  ngOnInit() {
-    const categGet = this.route.snapshot.paramMap.get('categ');
-    if (categGet) {
-      this.categ = Number(categGet);
+  constructor(
+    public route: ActivatedRoute,
+    public router: Router,
+    public msgCateg: MsgCateg,
+    private location: Location,
+    ) { }
+    categoria = this.msgCateg.categoria;
+    categorias = this.msgCateg.captarCategorias();
+  
+    ngOnInit() {
+      const categGet = this.route.snapshot.paramMap.get('categ');
+      if (categGet) {
+        this.categ = Number(categGet);
+        [this.filterObj] = this.categorias.filter((e) => e.id === this.categ);
+        this.arr = this.arrOpt.filter((e) => e.id !== this.categ);
+      }
     }
-  }
+  
+     /* functions necessary for reload the page by new params */
+    goBack(): void {
+      this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.location.back();
+      });
+    }
+  
+    navigateToA(): void{
+      if(this.arr){
+        const newRoute= this.arr[0].url;
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          this.router.navigate([newRoute]);
+        });
+      }
+     }
+     navigateToB(): void{
+      if(this.arr){
+        const newRoute= this.arr[1].url;
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          this.router.navigate([newRoute]);
+        });
+      }
+     }
+  
 }
