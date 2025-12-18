c11d24891e536079f09f606c2211002b384c862b
manelly67 <manellylopezqqq@gmail.com>
2025-12-18 16:56:53 -0500

    add esfera component

--- src/app/esfera/esfera.ts
+++ src/app/esfera/esfera.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-esfera',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './esfera.html',
   styleUrl: './esfera.css',
 })
 export class Esfera {
+@Input() ix?:number;
 
 }
